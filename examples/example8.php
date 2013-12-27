9e926542ca25c0e14e55daa3080387c4b15aebb2
Ken Guest <ken@linux.ie>
2013-12-27 10:57:56 +0000

    example of loading local data.

---
+++ examples/example8.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * example2_loadandparselocaldata.php
+ * 22-Nov-2009
+ *
+ * PHP Version 5
+ *
+ * @category Services
+ * @package  Services_OpenStreetMap
+ * @author   Ken Guest <kguest@php.net>
+ * @license  BSD http://www.opensource.org/licenses/bsd-license.php
+ * @version  Release: @package_version@
+ * @link     osmx.php
+ */
+
+$version = '@package_version@';
+if (strstr($version, 'package_version')) {
+    set_include_path(dirname(dirname(__FILE__)) . ':' . get_include_path());
+}
+
+require_once 'Services/OpenStreetMap.php';
+
+$osm = new Services_OpenStreetMap();
+
+$osm->loadXML("./ballinacurragardens.osm");
+$results = $osm->search(array("building" => "yes"));
+var_dump($results);
+
+// vim:set et ts=4 sw=4:
+?>
