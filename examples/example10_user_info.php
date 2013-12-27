1de198bd54da120dd4bc6ea3a1f8995a3791be5c
Ken Guest <ken@linux.ie>
2013-12-27 10:21:04 +0000

    Some examples re checkstatus and user info.

---
+++ examples/example10_user_info.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * example10.php
+ * 02-Oct-2012
+ *
+ * PHP Version 5
+ *
+ * @category Services
+ * @package  Services_OpenStreetMap
+ * @author   Ken Guest <kguest@php.net>
+ * @license  BSD http://www.opensource.org/licenses/bsd-license.php
+ * @version  Release: @package_version@
+ * @link     example10_user_info.php
+ */
+
+$version = '@package_version@';
+if (strstr($version, 'package_version')) {
+    set_include_path(dirname(dirname(__FILE__)) . ':' . get_include_path());
+}
+
+require_once 'Services/OpenStreetMap.php';
+
+$id = 1;
+
+$osm = new Services_OpenStreetMap();
+$user = $osm->getUserById($id);
+
+if ($user === false) {
+    die("User #$id not found\n");
+}
+
+echo "Display Name: ", $user->getDisplayName(), " (", $user->getId(),")\n";
+echo "Roles "; var_dump($user->getRoles()); echo "\n";
+echo "#Changesets ", $user->getChangesets(), "\n";
+echo "#Traces ", $user->getTraces(), "\n";
+echo "#BlocksReceived ", $user->getBlocksReceived(), "\n";
+echo "#ActiveBlocksReceived ", $user->getActiveBlocksReceived(), "\n";
+echo "#BlocksIssued ", $user->getBlocksIssued(), "\n";
+echo "#Languages "; var_dump($user->getLanguages()); echo "\n";
+?>
---
+++ examples/example11_checkstatus.php
@@ -0,0 +1,32 @@
+#!/usr/bin/php
+<?php
+/**
+ * Check API/DB/GPX server status
+ * 28 May 2012
+ *
+ * PHP Version 5
+ *
+ * @category Services
+ * @package  Services_OpenStreetMap
+ * @author   Ken Guest <kguest@php.net>
+ * @license  BSD http://www.opensource.org/licenses/bsd-license.php
+ * @link     example11_checkstatus.php
+ */
+
+$version = '@package_version@';
+if (strstr($version, 'package_version')) {
+    set_include_path(dirname(dirname(__FILE__)) . ':' . get_include_path());
+}
+
+require_once 'Services/OpenStreetMap.php';
+
+$config = array(
+    'server' => 'http://api.openstreetmap.org/',
+);
+
+$osm = new Services_OpenStreetMap($config);
+
+echo "Querying {$config['server']}\n\n";
+echo "API Status: " , $osm->getApiStatus(), "\n";
+echo "DB  Status: " , $osm->getDatabaseStatus(), "\n";
+echo "GPX Status: " , $osm->getGpxStatus(), "\n";
