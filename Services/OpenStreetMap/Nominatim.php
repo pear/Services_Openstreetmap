84829bff7737db319c3bd20bf0f2830ba2ca890c
Ken Guest <ken@linux.ie>
2014-10-05 16:45:20 +0100

    subpackage tags...

--- Services/OpenStreetMap/Nominatim.php
+++ Services/OpenStreetMap/Nominatim.php
@@ -5,21 +5,23 @@
  *
  * PHP Version 5
  *
- * @category Services
- * @package  Services_OpenStreetMap
- * @author   Ken Guest <kguest@php.net>
- * @license  BSD http://www.opensource.org/licenses/bsd-license.php
- * @link     Nominatim.php
+ * @category   Services
+ * @package    Services_OpenStreetMap
+ * @subpackage Services_OpenStreetMap_Nominatim
+ * @author     Ken Guest <kguest@php.net>
+ * @license    BSD http://www.opensource.org/licenses/bsd-license.php
+ * @link       Nominatim.php
  */
 
 /**
  * Services_OpenStreetMap_Nominatim
  *
- * @category Services
- * @package  Services_OpenStreetMap
- * @author   Ken Guest <kguest@php.net>
- * @license  BSD http://www.opensource.org/licenses/bsd-license.php
- * @link     Nominatim.php
+ * @category   Services
+ * @package    Services_OpenStreetMap
+ * @subpackage Services_OpenStreetMap_Nominatim
+ * @author     Ken Guest <kguest@php.net>
+ * @license    BSD http://www.opensource.org/licenses/bsd-license.php
+ * @link       Nominatim.php
  */
 class Services_OpenStreetMap_Nominatim
 {
