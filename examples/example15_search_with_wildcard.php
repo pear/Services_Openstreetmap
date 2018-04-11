8fa52eb5aecea805c3709139545d4112ab671ec3
Ken Guest <kguest@php.net>
2018-04-11 20:02:29 +0100

    Issue 22: Wildcard search - add example file.

---
+++ examples/example15_search_with_wildcard.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * example15_search_with_wildcard.php
+ * 11-Apr-2018
+ *
+ * PHP Version 5
+ *
+ * @category Services
+ * @package  Services_OpenStreetMap
+ * @author   Ken Guest <kguest@php.net>
+ * @license  BSD http://www.opensource.org/licenses/bsd-license.php
+ * @version  Release: @package_version@
+ * @link     osmx.php
+ */
+
+$version = '@package_version@';
+if (strstr($version, 'package_version')) {
+    set_include_path(dirname(dirname(__FILE__)) . PATH_SEPARATOR . get_include_path());
+}
+
+require_once 'Services/OpenStreetMap.php';
+
+$osm = new Services_OpenStreetMap();
+
+$osm->loadXML("./ballinacurragardens.osm");
+$results = $osm->search(array("building" => "*"));
+var_dump($results);
+
+// vim:set et ts=4 sw=4:
+?>
